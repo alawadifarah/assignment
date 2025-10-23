76d7bbdea72a8f9b4f3f649007214b852ac75d03
MOI <alawadifarah>
2025-10-23 15:11:09 +0300

    part 2 completed. added CommandCenter class and refactored code.

---
+++ ConsoleApp1/CommandCenter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CommandCenter
+    {
+        public string UserName { get; private set; } = "Guest";
+        public bool IsOperational { get; private set; } = true;
+   
+        public CommandCenter()
+        {
+            UserName = "Guest";
+            IsOperational = true;
+            
+        }
+
+        public void ShowHelp()
+        {
+            Console.WriteLine("Available Commands:");
+            Console.WriteLine("help - Show available commands");
+            Console.WriteLine("status - Check the current status");
+            Console.WriteLine("set_name - Change your name");
+            Console.WriteLine("exit - Close the program");
+        }
+
+        public void Run()
+        {
+            while (IsOperational)
+            {
+                Console.WriteLine("Enter command or type 'help' for options. ");
+                string command = Console.ReadLine().ToLower();
+
+
+                switch (command)
+                {
+                    case "help":
+                        ShowHelp();
+                        break;
+                    case "status":
+                        Console.WriteLine("Command Center is Operational.");
+                        Console.WriteLine("Current user:" + UserName);
+                        break;
+                    case "set_name":
+                        Console.WriteLine("Enter new name: ");
+                        UserName = Console.ReadLine();
+                        Console.WriteLine("Name updated! Hello " + UserName + ".");
+                        break;
+                    case "exit":
+                        Console.WriteLine("Goodbye!" + UserName + ".");
+                        IsOperational = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid command. Type 'help' for options.");
+                        break;
+                }
+
+            }
+        }
+
+    }
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,50 +11,10 @@ namespace assignmentPart1
     {
         static void Main(string[] args)
         {
-            string userName = "Guest";
+            
+            CommandCenter center = new CommandCenter();
+            center.Run();
 
-            Console.WriteLine("Welcome to Command Center!");
-            bool running = true;
-            while (running)
-            {
-                Console.WriteLine("Enter command or type 'help' for options. ");
-                string command = Console.ReadLine().ToLower();
-
-                switch (command)
-                {
-                    case "help":
-                        Console.WriteLine("Available Commands:");
-                        Console.WriteLine("help - Show available commands");
-                        Console.WriteLine("status - Check the current status");
-                        Console.WriteLine("set_name - Change your name");
-                        Console.WriteLine("exit - Close the program");
-                        break;
-
-                    case "status":
-                        Console.WriteLine("Command Center is Operational.");
-                        Console.WriteLine("Current user:" + userName);
-                        break;
-
-                    case "set_name":
-                        Console.WriteLine("Enter new name: ");
-                        userName = Console.ReadLine();
-                        Console.WriteLine("Name updated! Hello " + userName + ".");
-                        break;
-
-                    case "exit":
-                        Console.WriteLine("Goodbye!" + userName + ".");
-                        running = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("invalid command. Type 'help' for options.");
-                        break;
-
-
-                }
-
-
-            }
         }
     }
 }
